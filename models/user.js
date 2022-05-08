f57bce0257fa1293ee655b6b0829dad62237fee5
Dom <Domalbano35@gmail.com>
2022-05-08 19:40:33 -0400

    init

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      
+    },
+    score: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = assignmentSchema;
