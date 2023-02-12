35bbeadbf9e9c309414a0df3a204781938a3b0e3
dominick <domalbano35@gmail.com>
2023-02-12 15:39:48 -0500

    env vars

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const { connect, connection } = require('mongoose');
+require('dotenv').config();
 
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/letConnectDB';
+  process.env.DATABASE_URL ;
 
 connect(connectionString, {
   useNewUrlParser: true,
